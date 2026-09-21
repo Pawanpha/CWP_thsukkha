177e381a2c641389dddc404c5f54c9873949b688
Pawanpha <thisuk.110145@gmail.com>
2026-09-22 02:00:59 +0700

    Add calculator.py to perform basic arithmetic operations

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(str(a) + " + " + str(b) + " = " + str(a + b))
+print(str(a) + " - " + str(b) + " = " + str(a - b))
+print(str(a) + " / " + str(b) + " = " + str(a / b))
+print(str(a) + " * " + str(b) + " = " + str(a * b))
