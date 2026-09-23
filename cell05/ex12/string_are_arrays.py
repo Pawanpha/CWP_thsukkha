7fa4a2191e06dd7080bc5b49170c7c15990f66d5
Pawanpha <thisuk.110145@gmail.com>
2026-09-24 00:53:28 +0700

    Add string_are_arrays.py to count occurrences of 'z' in command-line input

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    string = sys.argv[1]
+    count = string.count('z')
+    if count == 0:
+        print("none")
+    else:
+        print('z' * count)
